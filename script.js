81aac4df4bbce923145383dd431d0ce457ada97a
dpxn23 <davidxuan.nguyen01@gmail.com>
2020-10-29 18:57:28 -0400

    second commit

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let companyName = 'Gucci'
 
 console.log(`You will be a ${jobTitle} in ${myLocation} , making $${annualSalary} for ${companyName}`)
 
+let eleOne = document.querySelector(`.ex1`)
+eleOne.textContent = `You will be a ${jobTitle} in ${geoLocation}, making $${annualSalary} for ${companyName}.`
+
 //EXERCISE 2: The Age Calculator
 //Forgot how old someone is? Calculate it! Store the current year in a variable. Store their birth year in a variable. Calculate their age based on the stored values. Output them to the console like so: "They are NN years old.", substituting the values.
 
@@ -17,6 +20,9 @@ let yearsOld = yearN - birthYear
 
 console.log(`They are ${yearsOld} years old.`)
 
+let eleTwo = document.querySelector(`#ex2`)
+eleTwo.textContent = `They are ${age} years old.`
+
 //EXERCISE 3: The Lifetime Supply Calculator
 //Ever wonder how much a "lifetime supply" of your favorite snack is? Wonder no more! Store your current age into a variable. Store a maximum age into a variable. Store an estimated amount per day (as a number). Calculate how many you would eat total for the rest of your life. Output the result to the console like so: "You will need NN to last you until the ripe old age of X".
 
@@ -24,7 +30,10 @@ let age = '23'
 let maxAge = '100'
 let perDay =  '3'
 
-console.log(`You will need ${(maxAge-age)*perDay} to last you until the ripe old age of ${maxAge}`)
+console.log(`You will need ${(maxAge-age)*perDay} snacks to last you until the ripe old age of ${maxAge}`)
+
+let eleThree = document.querySelector(`#ex3`)
+eleThree.textContent = `You will need ${(maxAge-age)*perDay} snacks to last you until the ripe old age of ${maxAge}`
 
 //EXERCISE 4: The Geometrizer
 //Calculate properties of a circle, using the definitions here. Store a radius into a variable. Calculate the circumference based on the radius, and output "The circumference is NN". Calculate the area based on the radius, and output "The area is NN".
@@ -34,6 +43,9 @@ const pi = Math.PI
 
 console.log(`The circumference is ${2 * pi * radius}`)
 console.log(`The area is ${pi * radius ** 2}`)
+
+let eleFour = document.querySelector(`#ex4`)
+eleFour.textContent = `The circumference is ${circumference}cm. The area is ${area}cm.`
 
 //EXERCISE 5: The Temperature Converter
 //It's hot out! Let's make a converter based on the steps here. Store a celsius temperature into a variable. Convert it to fahrenheit and output "NN°C is NN°F".  Now store a fahrenheit temperature into a variable. Convert it to celsius and output "NN°F is NN°C."
@@ -48,9 +60,12 @@ let c2 = (f2 - 32) * 5/9
 
 console.log(`${f2}°F is ${c2}°C`)
 
+let eleFive = document.querySelector(`#ex5`)
+eleFive.textContent = `${celsius}°C is ${fahrenheit}°F and ${fahrenheitTemp}°F is ${celciusTemp}°C.`
+
+// EXERCISE 6
+// Write a function named squareNumber that will take one argument 
+//(a number), square that number, and return the result. 
+//It should also log a string like "The result of squaring the number 3 is 9."
 
 
-
-
-let eleOne = document.querySelector(`#ex1`)
-eleOne.textContent = `You will be a ${jobTitle} in ${geoLocation}, making $${annualSalary} for ${companyName}.`
